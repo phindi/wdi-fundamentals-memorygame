f4031e5ca6b2b0acd623abc0eff587f4ce02f462
phindi <pindifuture@gmail.com>
2017-07-13 15:29:10 +0200

    created game logic

---
+++ memory_game/JS/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+
+ var cardOne = "king";
+ var cardTwo = "king";
+  var cardThree = "Queen";
+   var cardFour = "Queen";
+
+   console.log("User flipped " + cardOne);
